4dabefab6c9e71703fcfa7fd29fff5dfc42a2d63
CHollingworth <72136789+CHollingworth@users.noreply.github.com>
2022-02-14 09:44:17 +0000

    Cleanup

--- Assets/Scripts/bilboard.cs
+++ Assets/Scripts/bilboard.cs
@@ -6,6 +6,7 @@ public class bilboard : MonoBehaviour
 {
     void Update()
     {
+        //transform.rotation = Quaternion.LookRotation(transform.position, Camera.main.transform.position);
         transform.LookAt(-Camera.main.transform.position, Vector3.up);
     }
 }
