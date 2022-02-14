ae6d4af749ef840686ceb4fa0fbf1347750fcff7
JakeWarrenDavis <72136085+JakeWarrenDavis@users.noreply.github.com>
2022-02-14 08:35:58 +0000

    Removed fancy stuff
    minions use a rigidbody with constraints for collision

--- Assets/MinionAI.cs
+++ Assets/MinionAI.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class MinionAI : MonoBehaviour
 {
-    public float fallspeed = 1;
-    float yspeed = 0;
+    
     public MeshCollider meshCollider;
 
     // Start is called before the first frame update
@@ -17,24 +16,14 @@ public class MinionAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        yspeed -= fallspeed * Time.deltaTime;
-
-        //transform.position = transform.position + new Vector3(0, yspeed, 0);
-
-
-        Ray ray = new Ray();
-        ray.origin = transform.position + new Vector3(0, -1, 0);
-        ray.direction = new Vector3(0, -1, 0);
-
-        RaycastHit hitResult;
-
-        meshCollider.Raycast(ray, out hitResult, yspeed * -1);
-
-        if(hitResult.distance <= yspeed)
-        {
-            transform.position = hitResult.point;
-            yspeed = 0;
-        }
         
+
+
+        
+    }
+
+    void move_towards(float x, float z)
+    {
+        RigidBody body = 
     }
 }
