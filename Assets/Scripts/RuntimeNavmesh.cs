75fc937c6f2139292125b6a969d4bcf39742ea9a
j22-robertson <55785515+j22-robertson@users.noreply.github.com>
2022-02-14 12:53:58 +0000

    NavMeshbasics

---
+++ Assets/Scripts/RuntimeNavmesh.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RuntimeNavmesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+
+
+
+    // Use this for initialization
+    void Start () 
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
